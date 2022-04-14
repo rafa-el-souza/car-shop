37eb7fc377dcc4bb6e11d03b57e5080a279ba5d5
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-04-14 07:48:38 -0300

    refactor: zod schema messages

--- src/validations/CarSchema.ts
+++ src/validations/CarSchema.ts
@@ -7,15 +7,15 @@ const carSchema = vehicleSchema.extend({
       required_error: 'doorsQty is required',
       invalid_type_error: 'doorsQty must be a number',
     })
-    .gte(2)
-    .lte(4),
+    .gte(2, { message: 'doorsQty must be 2, 3 or 4' })
+    .lte(4, { message: 'doorsQty must be 2, 3 or 4' }),
   seatsQty: z
     .number({
       required_error: 'seatsQty is required',
       invalid_type_error: 'seatsQty must be a number',
     })
-    .gte(2)
-    .lte(7),
+    .gte(2, { message: 'seatsQty must be 2 to 7' })
+    .lte(7, { message: 'seatsQty must be 2 to 7' }),
 });
 
 export type Car = z.infer<typeof carSchema>;
--- src/validations/VehicleSchema.ts
+++ src/validations/VehicleSchema.ts
@@ -12,8 +12,8 @@ export const vehicleSchema = z.object({
       required_error: 'Year is required',
       invalid_type_error: 'Year must be a number',
     })
-    .gte(1900)
-    .lte(2022),
+    .gte(1900, { message: 'Year must be greater than or equal to 1900' })
+    .lte(2022, { message: 'Year must be less than or equal to 2022' }),
   color: z
     .string({
       required_error: 'Color is required',
