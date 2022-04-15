5e85a8596a9463c8bb6a57fa910d136a7d6a8c96
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-04-14 21:55:25 -0300

    feat: add error messages enum

---
+++ src/errors/ErrorMessages.ts
@@ -0,0 +1,9 @@
+export enum ErrorMessage {
+  notFound = 'Object not found',
+  internal = 'Something went wrong...',
+  invalidId = 'Id must have 24 hexadecimal characters',
+}
+
+export default {
+  ErrorMessage,
+};
