1714893f6b1d6d3e2ef88a79f0d10d18073a1377
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-04-14 22:00:15 -0300

    refactor: move methods to generic controller

--- src/controllers/CarController.ts
+++ src/controllers/CarController.ts
@@ -1,5 +1,3 @@
-import { NextFunction, Request, Response } from 'express';
-
 import { Controller as GenericController } from './GenericController';
 
 import { CarService } from '../services';
@@ -21,35 +19,6 @@ export class CarController extends GenericController<Car> {
   }
 
   get route() { return this._route; }
-
-  update = (
-    req: Request<{ id: string; }>,
-    res: Response<Car>,
-    next: NextFunction,
-  ): Promise<typeof res | void> => {
-    const { id } = req.params;
-    const { body } = req;
-    return this.service.update(id, body)
-      .then((result) => {
-        if (!result) return next({ code: 404, message: 'Object not found' });
-        return res.status(200).json(result);
-      })
-      .catch((error) => next({ error }));
-  };
-
-  delete = (
-    req: Request<{ id: string; }>,
-    res: Response<Car>,
-    next: NextFunction,
-  ): Promise<typeof res | void> => {
-    const { id } = req.params;
-    return this.service.delete(id)
-      .then((result) => {
-        if (!result) return next({ code: 404, message: 'Object not found' });
-        return res.status(200).json(result);
-      })
-      .catch((error) => next({ error }));
-  };
 }
 
 export default CarController;
--- src/controllers/GenericController.ts
+++ src/controllers/GenericController.ts
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import { ZodObject, ZodRawShape } from 'zod';
 
 import { Service as GenericService } from '../services';
+import { ErrorMessage as m } from '../errors';
+import { StatusCodes as c } from '../interfaces';
 
 export abstract class Controller<T> {
   abstract route: string;
@@ -13,7 +15,7 @@ export abstract class Controller<T> {
   ) {}
 
   validateBody = (
-    req: Request<T>,
+    req: Request<{ id: string; }>,
     res: Response<T>,
     next: NextFunction,
   ): void => {
@@ -34,20 +36,20 @@ export abstract class Controller<T> {
     const { id } = req.params;
     if (!mongoose.Types.ObjectId.isValid(id)) {
       return next({
-        code: 400, message: 'Id must have 24 hexadecimal characters',
+        code: c.badRequest, message: m.invalidId,
       });
     }
     return next();
   };
 
   create = (
-    req: Request<T>,
+    req: Request<{ id: string; }>,
     res: Response<T>,
     next: NextFunction,
   ): Promise<typeof res | void> => {
     const { body } = req;
     return this.service.create(body)
-      .then((result) => res.status(201).json(result))
+      .then((result) => res.status(c.created).json(result))
       .catch((error) => next({ error }));
   };
 
@@ -56,7 +58,7 @@ export abstract class Controller<T> {
     res: Response<T[]>,
     next: NextFunction,
   ): Promise<typeof res | void> => this.service.read()
-    .then((result) => res.status(200).json(result))
+    .then((result) => res.status(c.ok).json(result))
     .catch((error) => next({ error }));
 
   readOne = (
@@ -67,22 +69,39 @@ export abstract class Controller<T> {
     const { id } = req.params;
     return this.service.readOne(id)
       .then((result) => {
-        if (!result) return next({ code: 404, message: 'Object not found' });
-        return res.status(200).json(result);
+        if (!result) return next({ code: c.notFound, message: m.notFound });
+        return res.status(c.ok).json(result);
       })
       .catch((error) => next({ error }));
   };
 
-  abstract update(
-    req: Request<{ id: string; }, unknown, T>,
-    res: Response<T>,
-    next: NextFunction,
-  ): Promise<typeof res | void>;
-
-  abstract delete(
+  update = (
     req: Request<{ id: string; }>,
     res: Response<T>,
     next: NextFunction,
-  ): Promise<typeof res | void>;
+  ): Promise<typeof res | void> => {
+    const { id } = req.params;
+    const { body } = req;
+    return this.service.update(id, body)
+      .then((result) => {
+        if (!result) return next({ code: c.notFound, message: m.notFound });
+        return res.status(c.ok).json(result);
+      })
+      .catch((error) => next({ error }));
+  };
+
+  delete = (
+    req: Request<{ id: string; }>,
+    res: Response<Record<string, never>>,
+    next: NextFunction,
+  ): Promise<typeof res | void> => {
+    const { id } = req.params;
+    return this.service.delete(id)
+      .then((result) => {
+        if (!result) return next({ code: c.notFound, message: m.notFound });
+        return res.status(c.noContent).json({});
+      })
+      .catch((error) => next({ error }));
+  };
 }
 export default Controller;
