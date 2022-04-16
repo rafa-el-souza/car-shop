5027f334ca7d857be80da3cf1e9e2c10a7ef5abc
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-04-16 11:20:44 -0300

    refactor: change schema controller attribute to public

--- src/controllers/GenericController.ts
+++ src/controllers/GenericController.ts
@@ -11,7 +11,7 @@ export abstract class Controller<T> {
 
   constructor(
     protected service: GenericService<T>,
-    protected schema: ZodObject<ZodRawShape>,
+    public schema: ZodObject<ZodRawShape>,
   ) {}
 
   validateBody = (
