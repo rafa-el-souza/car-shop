84282e6f45bbb329b0ebd9a31c0bb9ee05dd2e1a
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-04-17 12:48:52 -0300

    test: add read cars integration tests

---
+++ src/tests/integration/02-readCars.test.ts
@@ -0,0 +1,54 @@
+import request from 'supertest';
+
+import connection from '../../connection';
+import { clearDatabase, closeDatabase } from './utils/db';
+
+import server from '../../server';
+
+import { createCarInput } from './utils/mocks';
+
+import { StatusCodes as c } from '../../interfaces';
+
+describe('02 - Endpoint GET /cars', () => {
+
+  beforeAll(async () => {
+    await connection();
+  });
+
+  beforeEach(async () => {
+    await clearDatabase();
+  });
+
+  afterAll(async () => {
+    await closeDatabase();
+  });
+
+  describe('✅ Success', () => {
+
+      it('Outputs code 200 and an array of cars', (done) => {
+        request(server.getApp())
+          .post('/cars')
+          .send(createCarInput)
+          .expect(c.created)
+          .then((createdCar) => {
+            request(server.getApp())
+              .get('/cars')
+              .expect(c.ok)
+              .then((res) => {
+                expect(res.body).toEqual([createdCar.body]);
+                return done();
+              })
+          });
+      })
+
+    it('Outputs code 200 and an empty array if there are no cars in the db', (done) => {
+      request(server.getApp())
+        .get('/cars')
+        .expect(c.ok)
+        .then((res) => {
+          expect(res.body).toEqual([]);
+          return done();
+        })
+    })
+  })
+});
