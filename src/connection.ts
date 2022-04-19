088ac8c8921ccf932d4f4e68a83077f1baa18f50
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-04-19 11:18:19 -0300

    chore: rename connect to database function

--- src/connection.ts
+++ src/connection.ts
@@ -2,9 +2,9 @@ import mongoose from 'mongoose';
 
 const MONGO_DB_URL = 'mongodb://localhost:27017/CarShop';
 
-const connectToDatabase = (
+const connection = (
   mongoDatabaseURI = process.env.MONGO_URI
     || MONGO_DB_URL,
 ) => mongoose.connect(mongoDatabaseURI);
 
-export default connectToDatabase;
+export default connection;
