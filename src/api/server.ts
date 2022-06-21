2fe3097e23fa6a9248906b8d83834370d94a4f0f
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-06-21 13:13:14 -0300

    feat: add swagger documentation server to server file

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,14 +1,19 @@
-import App from './api';
+import swaggerUi from 'swagger-ui-express';
+
+import Api from './api';
 
 import {
   handleDomainError,
   handleInternalError,
-  handleZodDomainError,
-} from '../app/helpers/errors';
+  handleZodDomainError } from './middlewares';
 
-import { CarFactory, MotorcycleFactory } from '../app/helpers/factories';
+import { CarFactory, MotorcycleFactory } from './helpers';
 
-const server = new App();
+import swaggerDocs from './helpers/swagger.json';
+
+const server = new Api();
+
+server.app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 server.addRouter(CarFactory().router);
 
